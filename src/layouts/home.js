17d8370d2a8241e65cc2867d935f0ed34daa552b
FelixIG15 <felix.ganumba@gmail.com>
2023-11-01 23:22:39 +0700

    checkpoint, testing container

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -1,9 +1,29 @@
 import React from "react";
+import UserProfileCard from "../components/caregiverCard";
+import { Container, Row, Col } from "react-bootstrap";
 
-function HomePage(){
-    return <div>
-        <h1>Home Page</h1>
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Container fluid>
+        <Row>
+          <Col>
+            <UserProfileCard />
+          </Col>
+          <Col>
+            <UserProfileCard />
+          </Col>
+          <Col>
+            <UserProfileCard />
+          </Col>
+          <Col>
+            <UserProfileCard />
+          </Col>
+        </Row>
+      </Container>
     </div>
+  );
 }
 
 export default HomePage;
