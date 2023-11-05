d0fdbdc0558eb797d4723113d9a38c73703e8fb9
Felix Ivander <felix.ganumba@gmail.com>
2023-11-05 22:39:13 +0700

    feat(src): add daterangepicker

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import DateRangePicker from "../components/dateRangePicker";
 
-function HomePage(){
-    return <div>
-        <h1>Home Page</h1>
+function HomePage() {
+
+  return (
+    <div>
+        <DateRangePicker/>
     </div>
+  );
 }
 
 export default HomePage;
